2f483c092e5f9ca90dc8c0c72b0694efd5d9fb31
Davide Marchegiani <davide.marchegiani@gmail.com>
2025-12-04 11:24:52 +1100

    Added day3

--- 2025/day3/main.py
+++ 2025/day3/main.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 import os 
 
-with open (os.path.join(os.path.dirname(__file__), 'input')) as f:
+with open (os.path.join(os.path.dirname(__file__), 'test_input')) as f:
     lines = f.read().splitlines()
 
-# part 1
-
-print(f'{p1=}')
-# print(f'{p2=}')
+# part 1 and 2
+n_b=[2,12]
+for N in n_b:
+    jolt=[]
+    for line in lines:
+        jlt=''
+        R=N
+        while R < len(line):
+            val = max(line[:-R+1])
+            jlt+=val
+            ind = line.index(val)
+            R-=1
+            line=line[ind+1:]
+            if R==1:
+                val = max(line)
+                jlt+=val
+                break
+        if R>1:
+            jlt+=line
+        jolt.append(int(jlt))
+    # print(jolt)
+    print(sum(jolt))
